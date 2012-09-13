9826c38df7fe68aff94cef2fafe4e4e2a8b76f77
ddyer <ddyer>
2012-09-13 19:22:59 +0000

    *** empty log message ***

---
+++ boardspace-html/htdocs/universe/UnivPuzzlesol_files/mc.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 2:36:00 Aug 30, 2009 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:07:39 May 4, 2012.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+var ap = window.navigator.appVersion.toLowerCase();
+var i = ap.indexOf("msie");
+if (ap.indexOf("windows") >= 0 && i >= 0 && ((i + 5) < ap.length)) {
+  var v = ap.charAt(i + 5);
+  if (v >= 5 ) {
+    document.write('<scr' + 'ipt src="http://web.archive.org/web/20090830023600/http://us.i1.yimg.com/us.yimg.com/i/mc/mc1.js"></scr' + 'ipt>');
+    document.write('<scr' + 'ipt src="http://web.archive.org/web/20090830023600/http://us.i1.yimg.com/us.yimg.com/i/mc/mc2.js"></scr' + 'ipt>');
+  }
+}
+
---
+++ boardspace-html/htdocs/universe/Universe2_files/geov2_001.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 4:59:09 Sep 2, 2009 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 18:12:33 May 4, 2012.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+var ycsdone;
+function geovisit()
+{
+  var z;
+  if (ycsdone)
+    return;
+  z="&r="+escape(document.referrer);
+  z=z+"&b="+escape(navigator.appName+" "+navigator.appVersion);
+  w=parseFloat(navigator.appVersion);
+  if (w > 2.0) {
+    z=z+"&s="+screen.width+"x"+screen.height;
+    z=z+"&o="+navigator.platform;
+    v="1.2";
+    if (navigator.appName != "Netscape") {
+      z=z+"&c="+screen.colorDepth;
+    } else {
+      z=z+"&c="+screen.pixelDepth
+    }
+    z=z+"&j="+navigator.javaEnabled();
+  } else {
+    v=1.0;
+  }
+  z=z+"&v="+v;
+  document.writeln("<img border=\"0\" style=\"display:none\" src=\"http://web.archive.org/web/20090902045909/http://visit.geocities.com/visit.gif?"+z+"\">");
+  ycsdone=1;
+}
